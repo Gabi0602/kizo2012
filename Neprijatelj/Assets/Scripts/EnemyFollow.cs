aa88e704de88eb182f70f9a564b5a6b5c62b31d7
Gabi0602 <gabrijel.krakic@gmail.com>
2021-03-18 16:47:26 +0100

    aaa

---
+++ Neprijatelj/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,26 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyFollow : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag=="bullet")
+        {
+            Debug.Log("yes");
+            Invoke("Destroy1",0.1f);
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void Destroy1()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Neprijatelj/Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 5f;
+    public Animator anim;
+    private Vector2 moveDirection;
+    public GameObject projectile;
+    private bool isRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+        Animate();
+
+        if (Input.GetKeyDown("f"))
+        {
+            var obj = Instantiate(projectile, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity) as GameObject;
+            obj.GetComponent<Projectile>().isRight = isRight;
+            if (isRight)
+            {
+                obj.transform.position = new Vector3(transform.position.x + 1.17f, transform.position.y, 0);
+            }
+            else
+            {
+                obj.transform.position = new Vector3(transform.position.x + 1.17f, transform.position.y, 0);
+            }
+        }
+    }
+    
+    private void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if ((moveX == 0 && moveY == 0) && moveDirection.x != 0 || moveDirection.y != 0)
+        {
+            Debug.Log(moveDirection.x + " " + moveDirection.y);
+        }
+        moveDirection = new Vector2(moveX, moveY);
+    }
+
+    private void Animate()
+    {
+        anim.SetFloat("AnimMoveX", moveDirection.x);
+        anim.SetFloat("AnimMoveY", moveDirection.y);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+    }
+
+    
+}
