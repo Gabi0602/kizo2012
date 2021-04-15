a7ac6adf589d2963a3d187a5ab3079ad9b7a9383
Gabi0602 <gabrijel.krakic@gmail.com>
2021-04-15 16:39:34 +0200

    eksplozija

---
+++ Neprijatelj/Assets/Scripts/BodyPart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Color start;
+    private Color end;
+    private float t;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        start = spriteRenderer.color;
+        end = new Color(start.r, start.g, start.b, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        spriteRenderer.color = Color.Lerp(start, end, t / 2);
+        if	(spriteRenderer.color.a <=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Neprijatelj/Assets/Scripts/Explode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public BodyPart bodyPart;
+    public int totalParts = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Deadly")
+        {
+            OnExplode();
+        }
+    }
+
+    private void OnExplode()
+    {
+        Destroy(gameObject);
+        var t = transform;
+        for (int i = 0; i < totalParts; i++)
+        {
+            BodyPart clone = Instantiate(bodyPart, t.position, Quaternion.identity) as BodyPart;
+            clone.rb.AddForce(Vector3.right * Random.Range(-50, 50));
+            clone.rb.AddForce(Vector3.up * Random.Range(100, 400));
+        }
+    }
+
+
+}
