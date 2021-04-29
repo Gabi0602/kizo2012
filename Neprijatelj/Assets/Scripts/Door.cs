607535e5d8db6ef7634d722a9987017ecde6960a
Gabi0602 <gabrijel.krakic@gmail.com>
2021-04-29 16:41:18 +0200

    Vrata

---
+++ Neprijatelj/Assets/Scripts/Door.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Animator anim;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            anim.SetInteger("AnimState", 1);
+        }
+        
+    }
+    
+}
